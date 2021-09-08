49a5da07456471357f93b0ffbeba6612e70dcc51
Rushiraj11 <grushiraj95@gmail.com>
2021-09-08 15:42:07 +0530

    Added: Creation of LinkedList

--- src/main/java/com/biz/linkedlist/LinkedListMain.java
+++ src/main/java/com/biz/linkedlist/LinkedListMain.java
@@ -3,6 +3,23 @@ package com.biz.linkedlist;
 public class LinkedListMain {
     public static void main(String[] args) {
         System.out.println("Welcome to Linkedlist program");
+        Node head;
+        //  Node tail;
+        Node firstNode = new Node(56);
+        Node secondNode = new Node(30);
+        Node thirdNode = new Node(70);
 
+        head = firstNode;
+        firstNode.next = secondNode;
+        secondNode.next = thirdNode;
+        // tail = thirdNode;
+
+        Node temp = head; // creating temp to store key value of head
+        while (temp != null) {
+            System.out.println(temp.data);
+            temp = temp.next;
+
+
+        }
     }
 }
